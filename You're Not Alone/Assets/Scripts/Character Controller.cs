a9dc5472c11d08aa4adf332d0b54677907a52f5a
JohnCornGames <joonatan.kytonen@gmail.com>
2022-08-22 14:40:38 +0300

    stuff

--- You're Not Alone/Assets/Scripts/Character Controller.cs
+++ You're Not Alone/Assets/Scripts/Character Controller.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    float speed;
+
+
 }

6f651291861859202c8881c65ceec75078415a0f
JohnCornGames <joonatan.kytonen@gmail.com>
2022-08-26 15:18:12 +0300

    stuff

---
+++ You're Not Alone/Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+    public GameObject mark;
+    public bool objectActive;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && objectActive)
+        {
+            SceneManager.LoadScene("Therapist office");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            objectActive = true;
+            mark.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            objectActive = false;
+            mark.SetActive(false);
+        }
+    }
+}
---
+++ You're Not Alone/Assets/Scripts/objects1.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class objects1 : MonoBehaviour
+{
+
+    public GameObject objectBox;
+    public GameObject mark;
+    public bool objectActive;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && objectActive)
+        {
+            if (objectBox.activeInHierarchy)
+            {
+                objectBox.SetActive(false);
+                PlayerMovement.playerControlsEnabled = true; 
+            }
+            else
+            {
+                objectBox.SetActive(true);
+                PlayerMovement.playerControlsEnabled = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            objectActive = true;
+            mark.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            objectActive = false;
+            objectBox.SetActive(false);
+            mark.SetActive(false);
+        }
+    }
+}
